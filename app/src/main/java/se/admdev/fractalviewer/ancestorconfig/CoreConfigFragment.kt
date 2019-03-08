4e85a880f5134d56546ced9c6f3544dae2d1e414
Adam Nilsson <adam.n.a3@gmail.com>
2019-03-08 20:34:34 +0100

    Add temporary node creation ui

--- app/src/main/java/se/admdev/fractalviewer/ancestorconfig/CoreConfigFragment.kt
+++ app/src/main/java/se/admdev/fractalviewer/ancestorconfig/CoreConfigFragment.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
+import kotlinx.android.synthetic.main.create_node_overlay.*
 import kotlinx.android.synthetic.main.fragment_core_config.*
 import se.admdev.fractalviewer.R
 import se.admdev.fractalviewer.ancestorconfig.model.AncestorTile
@@ -34,9 +36,7 @@ class CoreConfigFragment : Fragment(), AncestorTileAdapter.AncestorGridClickList
             (ancestor_grid.layoutManager as GridLayoutManager).spanCount = model.ancestorTileDimension
             adapter.setDataSet(items)
             adapter.notifyDataSetChanged()
-            val editMode = if (model.hasSelectedTile()) View.VISIBLE else View.GONE
-            dim.visibility = editMode
-            accept_selection_button.visibility = editMode
+            toggleNodeCreationMode()
         })
     }
 
@@ -68,12 +68,32 @@ class CoreConfigFragment : Fragment(), AncestorTileAdapter.AncestorGridClickList
             model.clearAncestorSelection()
         }
 
-        plus.setOnClickListener {
+        plus_grid_size_button.setOnClickListener {
             model.increaseAncestorTiles()
         }
 
-        minus.setOnClickListener {
+        minus_grid_size_button.setOnClickListener {
             model.decreaseAncestorTiles()
+        }
+
+        context?.let {
+            ArrayAdapter.createFromResource(
+                it,
+                R.array.test_ari,
+                android.R.layout.simple_spinner_item
+            ).also { adapter ->
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                operator_selection_spinner.adapter = adapter
+            }
+
+            ArrayAdapter.createFromResource(
+                it,
+                R.array.test_ari,
+                android.R.layout.simple_spinner_item
+            ).also { adapter ->
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                operand_selection_spinner.adapter = adapter
+            }
         }
     }
 
@@ -81,10 +101,13 @@ class CoreConfigFragment : Fragment(), AncestorTileAdapter.AncestorGridClickList
         // going via viewModel into the tile observer works but you lose recycler animation magic
         //        model.ancestorTiles.triggerObserver()
 
-        val editMode = if (model.hasSelectedTile()) View.VISIBLE else View.GONE
-        dim.visibility = editMode
-        accept_selection_button.visibility = editMode
+        toggleNodeCreationMode()
         adapter.notifyItemChanged(position)
+    }
+
+    private fun toggleNodeCreationMode() {
+        val editMode = if (model.hasSelectedTile()) View.VISIBLE else View.GONE
+        node_creation_controls.visibility = editMode
     }
 
     companion object {
