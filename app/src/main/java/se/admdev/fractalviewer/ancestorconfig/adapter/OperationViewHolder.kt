ad1bd7d8db2525ac2f321b431928c3215b38bb41
Adam Nilsson <adam.n.a3@gmail.com>
2019-03-17 16:06:48 +0100

    fix node list debugging formatting

--- app/src/main/java/se/admdev/fractalviewer/ancestorconfig/adapter/OperationViewHolder.kt
+++ app/src/main/java/se/admdev/fractalviewer/ancestorconfig/adapter/OperationViewHolder.kt
@@ -6,31 +6,43 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.list_item_operation_node.view.*
-import se.admdev.fractalviewer.R
+import se.admdev.fractalviewer.*
 import se.admdev.fractalviewer.ancestorconfig.model.ConfigNode
 import se.admdev.fractalviewer.ancestorconfig.model.OperationConfigNode
-import se.admdev.fractalviewer.getLabelColor
-import se.admdev.fractalviewer.gridLayoutManager
 
 class OperationViewHolder(itemView: View) : BindableViewHolder(itemView) {
 
     private val label: TextView = itemView.label
     private val operatorText: TextView = itemView.operator_text
+    private val operandText: TextView = itemView.operand_text
     private val grid: RecyclerView = itemView.ancestor_grid_miniature
     private var adapter: AncestorTileAdapter? = null
 
     override fun <T : ConfigNode> bind(node: T) {
         val n = node as OperationConfigNode
 
+        label.setBackgroundColor(n.label.getLabelColor())
+        label.text = n.label.toString()
+
         grid.gridLayoutManager.spanCount = n.tileSnapshot.size
         adapter?.setDataSet(n.tileSnapshot)
         adapter?.notifyDataSetChanged()
 
-        label.setBackgroundColor(n.label.getLabelColor())
-        label.text = n.label.toString()
+        if (n.operator != null && n.operand != null) {
+            operatorText.text = itemView.context.getString(R.string.node_list_sum_all_end_bracket, n.operator.symbol)
+            operandText.setVisible()
+            if(n.operand.label != null){
+                operandText.showLabel(n.operand.label)
+            } else {
+                operandText.text = n.operand.name
+                operandText.setBackgroundColor(0)
+            }
+        } else {
+            operatorText.text = itemView.context.getString(R.string.node_list_sum_all_end_bracket, "")
+            operandText.setGone()
+        }
 
-        operatorText.text =
-            itemView.context.getString(R.string.node_list_operator, n.operator?.symbol, n.operand?.name)
+
     }
 
     companion object {
