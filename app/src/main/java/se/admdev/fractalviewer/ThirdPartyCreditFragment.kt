876ba86c443669cd326834f7712f34e7e59d1038
Adam Nilsson <adam.n.a3@gmail.com>
2019-06-27 21:53:14 +0200

    Add gson license

--- app/src/main/java/se/admdev/fractalviewer/ThirdPartyCreditFragment.kt
+++ app/src/main/java/se/admdev/fractalviewer/ThirdPartyCreditFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_third_party_credits.*
 
 class ThirdPartyCreditFragment : Fragment() {
 
@@ -18,6 +19,21 @@ class ThirdPartyCreditFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        try {
+            val inputStream = resources.openRawResource(R.raw.license_gson) 
+            // :D from inputStream.available() docs:
+            // <p> Note that while some implementations of {@code InputStream } will return
+            // the total number of bytes in the stream, many will not.  It is
+            // never correct to use the return value of this method to allocate
+            // a buffer intended to hold all data in this stream.
+            val b = ByteArray(inputStream.available())
+            inputStream.read(b)
+            license_gson.text = String(b)
+        } catch (e: Exception) {
+            license_gson.text = ""
+        }
+
 
         val background = view.background as AnimationDrawable
         view.startBackgroundAnimation(background)
